a3825991b0099ac21fe6529be2803abd9ad73b7a
Adam Bien <abien@adam-bien.com>
2011-10-07 15:07:23 +0200

    JSF 2 admin panel panel introduced

--- lightfish/src/main/java/org/lightfish/presentation/administration/Index.java
+++ lightfish/src/main/java/org/lightfish/presentation/administration/Index.java
@@ -1,6 +1,9 @@
 package org.lightfish.presentation.administration;
 
 import javax.enterprise.inject.Model;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
@@ -9,5 +12,32 @@ import javax.enterprise.inject.Model;
 @Model
 public class Index {
     
+    @Size(min=5,max=30)
+    private String location;
+    
+    @Min(1)
+    private int interval;
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    
+    public Object changeAdministration(){
+        System.out.println(location + ":" + interval);
+        return null;
+    }
     
 }
