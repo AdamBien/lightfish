5c8a920d77abc9fa1ac5f2902e3881d107582622
Adam Bien <abien@adam-bien.com>
2012-01-30 20:20:06 +0100

    RESTConfig moved

--- lightfish/src/main/java/org/lightfish/business/RESTConfig.java
+++ lightfish/src/main/java/org/lightfish/business/RESTConfig.java
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package org.lightfish.business.monitoring.boundary;
+package org.lightfish.business;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
