620ac5b7b6eba7089fc04cc1258c0a7be2ce119e
Adam Bien <abien@adam-bien.com>
2012-03-25 16:29:22 +0200

    Initial creation of REST-interface to script management

---
+++ lightview/src/main/java/org/lightview/service/ScriptManager.java
@@ -0,0 +1,36 @@
+package org.lightview.service;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.GenericType;
+import java.util.List;
+import javax.ws.rs.core.MediaType;
+import org.lightview.model.Script;
+
+/**
+ *
+ * @author adam bien, adam-bien.com
+ */
+public class ScriptManager {
+ 
+    private String uri;
+    private final Client client;
+
+    public ScriptManager(String baseURI) {
+        this.uri = baseURI;
+        this.client = Client.create();
+    }
+    
+    
+    public List<Script> getAllScripts(){
+        GenericType<List<Script>> entities = new GenericType<List<Script>>(){};
+        return client.resource(this.uri).accept(MediaType.APPLICATION_XML).get(entities);
+    }
+    
+    public void registerNewScript(Script script){
+        client.resource(this.uri).accept(MediaType.APPLICATION_XML).put(script);
+    }
+    
+    public void removeScript(String scriptName){
+        client.resource(this.uri).delete();
+    }
+}
