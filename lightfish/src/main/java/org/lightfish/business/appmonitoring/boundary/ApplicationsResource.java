d06665f3bb587047f616931f6d926f9f27125053
Adam Bien <abien@adam-bien.com>
2013-08-26 12:31:57 +0200

    Structure for appmonitoring prepared

---
+++ lightfish/src/main/java/org/lightfish/business/appmonitoring/boundary/ApplicationsResource.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+package org.lightfish.business.appmonitoring.boundary;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author adam-bien.com
+ */
+@Path("applications")
+public class ApplicationsResource {
+}
