fe897f110311e8e56749facde0e28c5287be6dbe
Adam Bien <abien@adam-bien.com>
2013-08-26 17:07:53 +0200

    Accessing bean statistics works

---
+++ multilight/lightfish/src/main/java/org/lightfish/business/appmonitoring/control/EJBSensor.java
@@ -0,0 +1,37 @@
+/*
+ *
+ */
+package org.lightfish.business.appmonitoring.control;
+
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import javax.json.JsonObject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author adam-bien.com
+ */
+public class EJBSensor {
+
+    @Inject
+    Instance<String> location;
+
+    @Inject
+    Client client;
+
+    public JsonObject fetch(String applicationName, String ejbName, String methodName) {
+        WebTarget target = client.target(getUri() + "{application}/{bean}/bean-methods/{method}");
+        return target.resolveTemplate("application", applicationName).
+                resolveTemplate("bean", ejbName).
+                resolveTemplate("method", methodName).
+                request(MediaType.APPLICATION_JSON).get(JsonObject.class);
+    }
+
+    public String getUri() {
+        return "http://" + location.get() + "/monitoring/domain/server/applications/";
+    }
+
+}
---
+++ multilight/lightfish/src/test/java/org/lightfish/business/appmonitoring/control/EJBSensorIT.java
@@ -0,0 +1,39 @@
+/*
+ *
+ */
+package org.lightfish.business.appmonitoring.control;
+
+import javax.enterprise.inject.Instance;
+import javax.json.JsonObject;
+import javax.ws.rs.client.ClientBuilder;
+import org.glassfish.jersey.jsonp.JsonProcessingFeature;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author adam-bien.com
+ */
+public class EJBSensorIT {
+
+    EJBSensor cut;
+
+    @Before
+    public void init() {
+        this.cut = new EJBSensor();
+        this.cut.location = Mockito.mock(Instance.class);
+        this.cut.client = ClientBuilder.newClient().register(JsonProcessingFeature.class);
+    }
+
+    @Test
+    public void fetch() {
+        when(this.cut.location.get()).thenReturn("localhost:4848");
+        JsonObject methodStatistics = this.cut.fetch("lightfish", "Configurator", "getString-javax.enterprise.inject.spi.InjectionPoint");
+        Assert.assertNotNull(methodStatistics);
+        System.out.println("---- " + methodStatistics);
+    }
+
+}
