97c708626fbc50daf14afd3546ab777ffac00f2b
Adam Bien <abien@adam-bien.com>
2013-09-02 11:21:14 +0200

    Script for building, integration testing and launching added

--- multilight/lightview/buildAndStart.sh
+++ multilight/lightview/buildAndStart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./build.sh
+mvn failsafe:integration-test failsafe:verify
 ./lightview-app.sh
---
+++ multilight/lightview/src/main/java/org/lightview/presentation/methods/MethodsView.java
@@ -0,0 +1,7 @@
+package org.lightview.presentation.methods;
+
+/**
+ * @author: adam-bien.com
+ */
+public class MethodsView {
+}
