c881ad3c95cf63681e13927c7ec6504216731a97
Adam Bien <abien@adam-bien.com>
2013-09-04 19:57:48 +0200

    Status message incorporated

--- multilight/lightview/src/main/java/org/lightview/presentation/dashboard/toolbar/ToolbarPresenter.java
+++ multilight/lightview/src/main/java/org/lightview/presentation/dashboard/toolbar/ToolbarPresenter.java
@@ -1,9 +1,15 @@
 package org.lightview.presentation.dashboard.toolbar;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.FadeTransitionBuilder;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
+import javafx.util.Duration;
+import javafx.util.Pair;
+import org.lightview.business.administration.boundary.MonitoringLevelActivation;
 import org.lightview.business.administration.boundary.PollingSetup;
 
 import javax.inject.Inject;
@@ -13,7 +19,7 @@ import java.util.ResourceBundle;
 /**
  * @author: adam-bien.com
  */
-public class ToolbarPresenter implements Initializable{
+public class ToolbarPresenter implements Initializable {
 
     @FXML
     ToolBar toolbar;
@@ -24,8 +30,15 @@ public class ToolbarPresenter implements Initializable{
     @FXML
     public TextField interval;
 
+    @FXML
+    public Label message;
+
     @Inject
     PollingSetup administration;
+
+    @Inject
+    MonitoringLevelActivation monitoringLevelActivation;
+
 
 
     @Override
@@ -34,20 +47,57 @@ public class ToolbarPresenter implements Initializable{
     }
 
 
-    public void startMonitoring(){
+    public void startMonitoring() {
+        showProgress();
         final String location = this.location.getText();
         final String intervalText = this.interval.getText();
         final String nextTimeout = administration.changeInterval(location, Integer.valueOf(intervalText));
+        hideProgress();
+        status();
+    }
+
+    public void stopMonitoring() {
+        showProgress();
+        administration.stopPolling();
+        hideProgress();
+        status();
+    }
+
+    public void activateMonitoring() {
+        showProgress();
+        monitoringLevelActivation.activateMonitoring();
+        hideProgress();
+    }
+
+    public void deactivateMonitoring() {
+        showProgress();
+        monitoringLevelActivation.deactivateMonitoring();
+        hideProgress();
+    }
+
+    public void status() {
+        final Pair<String,String> status = administration.status();
+        showMessage();
+        message.setText("Interval: " + status.getKey() + " next timeout: " + status.getValue());
+    }
+
+
+
+    void showMessage() {
+        final FadeTransition fadeTransition = new FadeTransition(Duration.seconds(3),this.message);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1.0);
+        fadeTransition.setAutoReverse(true);
+        fadeTransition.setCycleCount(2);
+        fadeTransition.play();
 
     }
 
-    public void stopMonitoring(){
-       administration.stopPolling();
+    void showProgress() {
     }
 
-    public void activateMonitoring(){}
+    void hideProgress() {
+    }
 
-    public void deactivateMonitoring(){}
 
-    public void status(){}
 }
