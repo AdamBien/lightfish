e560f60fbab92288ff86300271cabb50d0701de6
Adam Bien <abien@adam-bien.com>
2013-09-05 20:23:59 +0200

    Version number fetched from pom.xml

--- multilight/lightfish/src/main/java/org/lightfish/business/administration/boundary/ServerInfoResource.java
+++ multilight/lightfish/src/main/java/org/lightfish/business/administration/boundary/ServerInfoResource.java
@@ -3,13 +3,19 @@
  */
 package org.lightfish.business.administration.boundary;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import org.lightfish.business.servermonitoring.boundary.ServerInformation;
 import org.lightfish.business.servermonitoring.entity.OneShot;
 
@@ -23,6 +29,9 @@ public class ServerInfoResource {
 
     @Inject
     ServerInformation information;
+
+    @Context
+    ServletContext sc;
 
     @GET
     public JsonObject info() {
@@ -40,4 +49,23 @@ public class ServerInfoResource {
         return builder.build();
     }
 
+    @GET
+    @Path("lightfish")
+    public JsonObject lightfish() {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        Properties properties = new Properties();
+        InputStream is = this.sc.getResourceAsStream("/META-INF/maven/org.glassfish/lightfish/pom.properties");
+        if (is != null) {
+            try {
+                properties.load(is);
+            } catch (IOException ex) {
+                throw new IllegalStateException("Cannot load properties: " + ex, ex);
+            }
+        }
+        Set<Object> keySet = properties.keySet();
+        for (Object object : keySet) {
+            builder.add((String) object, (String) properties.get(object));
+        }
+        return builder.build();
+    }
 }
