15de2beb9cadfffb76d778a7091fbaa78c921840
Adam Bien <abien@adam-bien.com>
2015-11-13 09:37:17 +0100

    Mimetype specified, cleanup

--- multilight/lightfish/src/main/java/org/lightfish/business/administration/boundary/PollingResource.java
+++ multilight/lightfish/src/main/java/org/lightfish/business/administration/boundary/PollingResource.java
@@ -9,10 +9,13 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import org.lightfish.business.configuration.boundary.Configurator;
 import org.lightfish.business.servermonitoring.boundary.MonitoringAdmin;
 import org.lightfish.business.servermonitoring.boundary.MonitoringController;
@@ -22,6 +25,8 @@ import org.lightfish.business.servermonitoring.boundary.MonitoringController;
  * @author adam-bien.com
  */
 @Path("polling")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 @Stateless
 public class PollingResource {
 
@@ -37,7 +42,6 @@ public class PollingResource {
 
     @POST
     public JsonObject startPolling(JsonObject pollingInfo) {
-        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
         int interval = pollingInfo.getInt(INTERVAL);
         configurator.setValue(INTERVAL, interval);
         String location = pollingInfo.getString(LOCATION, "localhost:4848");
